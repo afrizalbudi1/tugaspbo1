dfaeb2161021893933288c801297c3c7fe86c45a
afrzalbudi1 <32351812+afrizalbudi1@users.noreply.github.com>
2018-03-24 21:19:30 +0800

    membuat atribut dan behavior

--- src/tugaspbo1/Helm.java
+++ src/tugaspbo1/Helm.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tugaspbo1;
 
 /**
@@ -10,5 +6,21 @@ package tugaspbo1;
  * @author Asus
  */
 public class Helm {
-    
+  String merk;
+  String jenis;
+  String aksesoris;
+  String ukuran;
+  String harga;
+  
+  void cetakinfo(){
+      System.out.println("Merk Helm      : "+merk);
+      System.out.println("Jenis Helm     : "+jenis);
+      System.out.println("Aksesoris Helm : "+aksesoris);
+      System.out.println("Ukuran Helm    : "+ukuran);
+      System.out.println("Harga Helm     : "+harga);
+      System.out.println("╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩");
+      
+      
+  }
+  
 }
